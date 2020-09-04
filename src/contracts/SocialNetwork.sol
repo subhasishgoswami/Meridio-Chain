a074c1e903ae26e1f1b1083e32d18527e25a0895
Subhasish Goswami <subhasishgoswami00@gmail.com>
2020-09-04 17:59:54 +0530

    Smart Contracts added

---
+++ src/contracts/SocialNetwork.sol
@@ -0,0 +1,70 @@
+pragma solidity ^0.5.0;
+
+contract SocialNetwork {
+    string public name;
+    uint public postCount = 0;
+    mapping(uint => Post) public posts;
+    mapping(uint => Post) public user_post;
+    mapping(address => uint) public user_post_count;
+
+    struct Post {
+        uint id;
+        string heading;
+        string content;
+        string email;
+        uint tipAmount;
+        address payable author;
+    }
+
+    event PostCreated(
+        uint id,
+        string heading,
+        string content,
+        string email,
+        uint tipAmount,
+        address payable author
+    );
+
+    event PostTipped(
+        uint id,
+        string content,
+        uint tipAmount,
+        address payable author
+    );
+
+    constructor() public {
+        name = "Meridio Chain";
+    }
+
+    function createPost(string memory _heading, string memory _content, string memory _email) public {
+        // Require valid content
+        require(bytes(_content).length > 0);
+        // Increment the post count
+        postCount ++;
+        // Create the post
+        posts[postCount] = Post(postCount, _heading, _content, _email, 0, msg.sender);
+        user_post_count[msg.sender] ++;
+        user_post[user_post_count[msg.sender]] = Post(postCount, _heading, _content, _email, 0, msg.sender);
+        // Trigger event
+        emit PostCreated(postCount, _heading, _content, _email, 0, msg.sender);
+    }
+
+    function tipPost(uint _id) public payable {
+        // Make sure the id is valid
+        require(_id > 0 && _id <= postCount);
+        // Fetch the post
+        Post memory _post = posts[_id];
+        // Fetch the author
+        address payable _author = _post.author;
+        // Pay the author by sending them Ether
+        address(_author).transfer(msg.value);
+        // Incremet the tip amount
+        _post.tipAmount = _post.tipAmount + msg.value;
+        // Update the post
+        posts[_id] = _post;
+        // Trigger an event
+        emit PostTipped(postCount, _post.content, _post.tipAmount, _author);
+    }
+
+    
+}
